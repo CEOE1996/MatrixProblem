fb4c17f0d76121bb0e791e76582f9571f07f2e25
Carlos Enrique Orozco Estrada <ceoe1996@hotmail.com>
2018-03-03 20:33:41 -0600

    Init Matrix Problem

---
+++ matrix.py
@@ -0,0 +1,9 @@
+n, m = input("").strip().split(' ')
+n, m = [int(n), int(m)]
+matrix = []
+matrix_i = 0
+for matrix_i in range(n):
+    matrix_t = str(input(""))
+    matrix.append(matrix_t)
+
+print(matrix)
