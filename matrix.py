1e5aac14eb26186db8db10dcdb5ac970cdb33818
Carlos Enrique Orozco Estrada <ceoe1996@hotmail.com>
2018-03-05 00:03:00 -0600

    Decoding Matrix

--- matrix.py
+++ matrix.py
@@ -1,3 +1,5 @@
+import re
+
 n, m = input("").strip().split(' ')
 n, m = [int(n), int(m)]
 matrix = []
@@ -6,4 +8,20 @@ for matrix_i in range(n):
     matrix_t = str(input(""))
     matrix.append(matrix_t)
 
-print(matrix)
+Decoded = ""
+
+for y in range(m):
+    for x in range(n):
+        Decoded += matrix[x][y]
+
+Start = re.findall('^\W+|$', Decoded)[0]
+Decoded = re.sub('^\W+|$', '', Decoded)
+
+End = re.findall('\W+$', Decoded)
+End.append("")
+Decoded = re.sub('\W+$', '', Decoded)
+
+Decoded = re.sub('\W+', ' ', Decoded)
+
+
+print(Start + Decoded + End[0])
